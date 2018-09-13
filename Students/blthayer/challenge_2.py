7f90f141a260f2d08f76546ef7a61f526a68fbff
Brandon Thayer <blthayer@tamu.edu>
2018-09-13 12:10:43 -0500

    Intermediate commit for challenge 2 - switching machines.

--- Students/blthayer/challenge_2.py
+++ Students/blthayer/challenge_2.py
@@ -5,6 +5,7 @@ import os
 # Installed packages.
 import numpy as np
 from sklearn.linear_model import LogisticRegression as LR
+import pandas as pd
 
 # Define mnist files. These were decompressed with 'gzip -d <file>'
 MNIST_DIR = 'mnist'
@@ -63,25 +64,39 @@ if __name__ == '__main__':
     seed = 42
 
     # Read training images and labels.
+    '''
     train_images = read_images(os.path.join(MNIST_DIR, TRAIN_IMAGES_FILE))
     train_labels = read_labels(os.path.join(MNIST_DIR, TRAIN_LABELS_FILE))
 
     # Read test data and labels.
     test_images = read_images(os.path.join(MNIST_DIR, TEST_IMAGES_FILE))
     test_labels = read_labels(os.path.join(MNIST_DIR, TEST_LABELS_FILE))
+    '''
+
+    # Read files from Prof. Chamberland
+    train_images = pd.read_csv(os.path.join(MNIST_DIR, 'mnist_train.csv'))
+    test_images = pd.read_csv(os.path.join(MNIST_DIR, 'mnist_test.csv'))
+
+    train_data = train_images.drop(['Id', 'Category'], axis=1)
+    train_labels = train_images['Category']
 
     # Initialize logistic regression object. Note: newton-cg solver failed to
     # converge within 100 iterations (the default).
     lr = LR(solver='newton-cg', multi_class='multinomial', max_iter=200)
 
     # Fit to the training data.
-    lr.fit(train_images, train_labels)
+    lr.fit(train_images.iloc, train_labels.iloc)
+
+    # Get the coefficients.
+    coeff = pd.DataFrame(lr.coef_)
+
+    coeff.to_csv('challenge2_coefficients.csv')
 
     # Predict from the test data, and score it.
-    mean_accuracy = test_output = lr.score(test_images, test_labels)
+    # mean_accuracy = test_output = lr.score(test_images, test_labels)
 
     # Inform.
-    print('Mean accuracy: {}'.format(mean_accuracy))
+    # print('Mean accuracy: {}'.format(mean_accuracy))
 
     '''
     import matplotlib.pyplot as plt
