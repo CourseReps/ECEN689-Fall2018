50c527f3472e27416c76d43278387d701974c731
mason-rumuly <masondatminer@tamu.edu>
2018-10-01 18:40:03 -0500

    gephy-ready files

---
+++ Students/mason-rumuly/challenge-03/graph_file_gen.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+# file names
+adjacency_mat_file = 'Students/mason-rumuly/challenge-03/3parameters_countries_only.csv'
+node_file = 'Students/mason-rumuly/challenge-03/nodes.csv'
+edge_file = 'Students/mason-rumuly/challenge-03/edges.csv'
+
+# import coefficient table
+adjacency_mat = pd.read_csv(adjacency_mat_file, index_col='Country Name', encoding='cp1252').dropna(axis=0)
+countries = list(enumerate(adjacency_mat.index.values))
+
+# Enumerate nodes
+with open(node_file, 'w') as f:
+    f.write('id;label\n')
+    for i, c in countries:
+        f.write('{};"{}"\n'.format(i, c))
+
+# Enumerate edges
+with open(edge_file, 'w') as f:
+    f.write('Source;Target;Label\n')
+    for source, _ in countries:
+        for sink, _ in countries:
+            if adjacency_mat.iloc[source, sink] != 0:
+                f.write('{};{};"{}"\n'.format(source, sink, adjacency_mat.iloc[source, sink]))
