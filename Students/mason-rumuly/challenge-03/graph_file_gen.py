2fac0d4d618ff82b342e95fa87e9bf08c7985f73
mason-rumuly <masondatminer@tamu.edu>
2018-10-02 09:57:50 -0500

    Fixed Graph Gen

--- Students/mason-rumuly/challenge-03/graph_file_gen.py
+++ Students/mason-rumuly/challenge-03/graph_file_gen.py
@@ -9,8 +9,10 @@ edge_file = 'Students/mason-rumuly/challenge-03/edges.csv'
 
 # import coefficient table
 adjacency_mat = pd.read_csv(adjacency_mat_file, index_col='Country Name', encoding='cp1252').dropna(axis=0)
-adjacency_mat = drop_non_countries(adjacency_mat)
+# adjacency_mat = drop_non_countries(adjacency_mat)
+# adjacency_mat = drop_non_countries(adjacency_mat.T).T
 countries = list(enumerate(adjacency_mat.index.values))
+countries_T = list(enumerate(adjacency_mat.index.T.values))
 
 # Enumerate nodes
 with open(node_file, 'w') as f:
@@ -21,7 +23,9 @@ with open(node_file, 'w') as f:
 # Enumerate edges
 with open(edge_file, 'w') as f:
     f.write('Source;Target;Label\n')
-    for source, _ in countries:
-        for sink, _ in countries:
+    for source, name_source in countries:
+        for sink, name_sink in countries_T:
             if adjacency_mat.iloc[source, sink] != 0:
+                if(source == sink):
+                    print(source, name_source, name_sink, adjacency_mat.iloc[source, sink])
                 f.write('{};{};"{}"\n'.format(source, sink, adjacency_mat.iloc[source, sink]))
