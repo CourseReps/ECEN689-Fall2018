102b8c21cf9429a3f5e60b79835821fb1c6e8279
mason-rumuly <masondatminer@tamu.edu>
2018-10-26 00:00:19 -0500

    Pearson Correlation

--- Projects/1ProjectFiles/Team8/ExploreRelation.py
+++ Projects/1ProjectFiles/Team8/ExploreRelation.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
-from RetrieveOriginalData import get_data
-from sklearn.linear_model import LinearRegression
 import numpy as np
+from scipy.stats import linregress
+
+from RetrieveOriginalData import get_data
+
 
 def scatter_with_trend(x, y, title=None, xlabel=None, ylabel=None, subplot=None):
-    lr = LinearRegression()
-    lr.fit(x, y)
+    lr = linregress(x.reshape(-1), y)
 
-    range_x = [min(x), max(x)] 
+    range_x = np.array([min(x), max(x)])
+    y_hat = lr.slope*range_x + lr.intercept
 
     if subplot is not None:
         plt.subplot(*subplot)
@@ -15,7 +17,9 @@ def scatter_with_trend(x, y, title=None, xlabel=None, ylabel=None, subplot=None)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.scatter(x, y, c='b')
-    plt.plot(range_x, lr.predict(range_x), c='r')
+    plt.plot(range_x, y_hat, c='r')
+
+    print(lr.rvalue, lr.pvalue/2) # want single-tailed version
     
 # load data
 health_med = get_data()
@@ -26,6 +30,7 @@ X -= X.min()
 X /= X.max()
 
 plt.figure()
+# strong for diabetes (|r| > 0.5)
 scatter_with_trend(
     X, health_med['PCT_DIABETES_ADULTS08'],
     '2008', 'Median Income (USD)', 'Diabetes Rate (%)', (2,2,1)
@@ -34,6 +39,7 @@ scatter_with_trend(
     X, health_med['PCT_DIABETES_ADULTS13'], 
     '2013', 'Median Income (USD)', 'Diabetes Rate (%)', (2,2,2)
 )
+# moderate for obesity (0.5 > |r| > 0.3)
 scatter_with_trend(
     X, health_med['PCT_OBESE_ADULTS08'],
     None, 'Median Income (USD)', 'Obesity Rate (%)', (2,2,3)
