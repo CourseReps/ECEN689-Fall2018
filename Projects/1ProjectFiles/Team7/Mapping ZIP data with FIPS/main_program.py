6aa70c9d664e860c4d146251b74f2f767586299d
shabarishkumar <shabarik@tamu.edu>
2018-10-28 01:16:28 -0500

    Information mapping Code added

---
+++ Projects/1ProjectFiles/Team7/Mapping ZIP data with FIPS/main_program.py
@@ -0,0 +1,11 @@
+from addFIPStoIncome import map_fips
+from aggregateAllZipsinFIPS import aggregate_zips
+from MapIncomeAndHealth import map_function
+
+map_fips('08zpall.csv', 'ZIP-COUNTY-FIPS_2017-06.csv', '2008_income_with_FIPS.csv')
+aggregate_zips('2008_FIPS_income_final.csv', '2008_income_seperated.csv')
+map_function('2008_income_seperated.csv', 'Diabeties_obese.csv', '2008_mapped_data.csv')
+
+map_fips('13zpallagi.csv', 'ZIP-COUNTY-FIPS_2017-06.csv', '2013_income_with_FIPS.csv')
+aggregate_zips('2013_FIPS_income_final.csv', '2013_income_seperated.csv')
+map_function('2013_income_seperated.csv', 'Diabeties_obese.csv', '2013_mapped_data.csv')
