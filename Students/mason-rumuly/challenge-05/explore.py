a8440b45a6899720e976d0d249cecfa5c14da220
mason-rumuly <masondatminer@tamu.edu>
2018-11-08 10:37:06 -0600

    Challenge 5 sans report

--- Students/mason-rumuly/challenge-05/explore.py
+++ Students/mason-rumuly/challenge-05/explore.py
@@ -4,12 +4,12 @@ from inspect import getsourcefile
 import numpy as np
 from sklearn.utils import shuffle
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_val_score, LeaveOneOut
 import matplotlib.pyplot as plt
 
 homeDir = os.path.sep.join(os.path.abspath(getsourcefile(lambda:0)).split(os.path.sep)[:-4]) + os.path.sep
 sourceDir = homeDir + 'Challenges{0}5Files{0}'.format(os.path.sep)
-targetDir = homeDir
+targetDir = homeDir + 'Students{0}mason-rumuly{0}challenge-05{0}'.format(os.path.sep)
 
 kernels = {
     'Linear':('linear', 1),
@@ -39,7 +39,7 @@ if __name__=='__main__':
     best = [None, 0]
     for k in kernels:
         print(k)
-        result = sum(cross_validate(
+        result = cross_val_score(
             SVC(
                 kernel=kernels[k][0],
                 gamma='scale',
@@ -47,19 +47,21 @@ if __name__=='__main__':
             ), 
             training[['Feature 0', 'Feature 1']], 
             training['Class'],
-            cv=cv_bins,
+            cv=LeaveOneOut(),  # cv_bins,
             n_jobs=-1,
             verbose=0
-        )['test_score'])/cv_bins
-        if result > best[1]:
+        )
+        score = sum(result)/len(result)
+
+        if score > best[1]:
             best[0] = k
-            best[1] = result
-        print(result)
+            best[1] = score
+        print(score)
     print('best kernel:', best[0])
 
     # train best on all data
     svc = SVC(
-        kernel=kernels[best[0]][0], 
+        kernel=kernels[best[0]][0],
         degree=kernels[best[0]][1],
         gamma='scale'
     )
@@ -80,8 +82,32 @@ if __name__=='__main__':
     # plot
     plt.figure()
     # decision contours
-    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.5)
     # points
-    plt.scatter(training['Feature 0'], training['Feature 1'], c=training['Class'])
-    plt.scatter(testing['Feature 0'], testing['Feature 1'])
+    plt.scatter(training.loc[training['Class'] == 0]['Feature 0'], training[training['Class'] == 0]['Feature 1'], label='Class 0', c='blue')
+    plt.scatter(training.loc[training['Class'] == 1]['Feature 0'], training[training['Class'] == 1]['Feature 1'], label='Class 1', c='red')
+    plt.savefig(targetDir + 'Regions.png')
+    # labels
+    plt.xlabel('Feature 0')
+    plt.ylabel('Feature 1')
+    plt.title('Training Set and Decision Regions')
+    plt.legend()
     plt.show()
+
+    # enter predictions
+    testing['Class'] = svc.predict(testing[['Feature 0','Feature 1']])
+    testing.to_csv(sourceDir + '5challenge_testing_masondatminer.csv')
+
+    # # plot predictions for sanity check
+    # plt.figure()
+    # # decision contours
+    # plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.5)
+    # # points
+    # plt.scatter(testing.loc[testing['Class'] == 0]['Feature 0'], testing[testing['Class'] == 0]['Feature 1'], label='Class 0', c='blue')
+    # plt.scatter(testing.loc[testing['Class'] == 1]['Feature 0'], testing[testing['Class'] == 1]['Feature 1'], label='Class 1', c='red')
+    # # labels
+    # plt.xlabel('Feature 0')
+    # plt.ylabel('Feature 1')
+    # plt.title('Testing Set and Decision Regions')
+    # plt.legend()
+    # plt.show()
